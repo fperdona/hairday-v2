675ff069721aa440ba4434a1880de52f13f0fcd6
Francisco Perdona <francisco.perdona@gmail.com>
2026-01-10 06:41:09 -0300

    Criar componente Icon com suporte a SVG via vite-plugin-svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import Text from "./core-components/text";
+import Icon from "./core-components/icon";
+import SunHorizon from "./assets/icons/sun-horizon.svg?react";
+import CloudSun from "./assets/icons/cloud-sun.svg?react";
+import UserSquare from "./assets/icons/user-square.svg?react";
+import MoonStars from "./assets/icons/moon-stars.svg?react";
+import CaretDown from "./assets/icons/caret-down.svg?react";
+import CalendarBlank from "./assets/icons/calendar-blank.svg?react";
+import CaretRight from "./assets/icons/caret-right.svg?react";
+import CaretLeft from "./assets/icons/caret-left.svg?react";
+import Trash from "./assets/icons/trash.svg?react";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 p-4">
-      <Text variant="body-lg-bold" as="h1">
-        Hairday
-      </Text>
-      <Text variant="body-md">Sistema de agendamento</Text>
-      <Text variant="body-sm" className="text-gray-400">
-        Versão 2.0
-      </Text>
+    <div className="min-h-screen bg-gray-900 p-4 flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-lg-bold">Title Lg</Text>
+        <Text variant="body-md-bold">Title Md</Text>
+        <Text variant="body-sm-bold">Title Sm</Text>
+        <Text variant="body-md">Title Md</Text>
+        <Text>Title Sm</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={SunHorizon} className="fill-yellow" />
+        <Icon svg={CloudSun} className="fill-yellow" />
+        <Icon svg={UserSquare} className="fill-yellow" />
+        <Icon svg={MoonStars} className="fill-yellow" />
+        <Icon svg={CaretDown} className="fill-yellow" />
+        <Icon svg={CalendarBlank} className="fill-yellow" />
+        <Icon svg={CaretRight} className="fill-yellow" />
+        <Icon svg={CaretLeft} className="fill-yellow" />
+        <Icon svg={Trash} className="fill-yellow" />
+      </div>
     </div>
   );
 }
